945b7673a134b30ec36a2d05c59448802745a2b2
Ajarek <ajarek@poczta.onet.pl>
2024-08-27 00:41:02 +0200

    added chartPie

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,96 @@
 import { auth } from '@/app/api/auth/auth'
-
+import transactions from '@/data/data.json'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
-
-
+import { ChevronRight } from 'lucide-react'
+import { ChartPie } from '@/components/ChartPie'
 
 const Dashboard = async () => {
   const session = await auth()
-  if(!session){
+  if (!session) {
     redirect('/')
   }
-  
+  const income = transactions
+    .filter((t) => t.type === 'income')
+    .reduce((acc, t) => acc + t.amount, 0)
+  const expense = transactions
+    .filter((t) => t.type === 'expense')
+    .reduce((acc, t) => acc + t.amount, 0)
+  const saved = transactions
+    .filter((t) => t.saved > 0)
+    .reduce((acc, t) => acc + t.saved, 0)
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start px-24 max-sm:px-4 py-8 gap-8">
-      <h1 className="text-3xl font-bold">Dashboard</h1>
-        
-    </div>
+    <main className='flex min-h-screen flex-col items-center justify-start px-24 max-sm:px-4 py-8 gap-8'>
+      <div className='w-full grid grid-cols-3 gap-6'>
+        <div className='border-2 border-blue-500 rounded-lg p-4'>
+          <h2 className='text-xl'>Current Balance</h2>
+          <span className='text-2xl font-semibold'>
+            ${(income - expense).toFixed(2)}
+          </span>
+        </div>
+        <div className='border-2 border-green-500 rounded-lg p-4'>
+          <h2 className='text-xl'>Income</h2>
+          <span className='text-2xl font-semibold'>${income.toFixed(2)}</span>
+        </div>
+        <div className='border-2 border-red-500 rounded-lg p-4'>
+          <h2 className='text-xl'>Expense</h2>
+          <span className='text-2xl font-semibold'>${expense.toFixed(2)}</span>
+        </div>
+      </div>
+
+      <div className='w-full grid grid-cols-[2fr_1fr] gap-6'>
+        <div className='flex flex-col'>
+          <div className='flex items-center justify-between py-2'>
+            <h1 className='text-xl font-semibold'>Pots</h1>
+            <Link
+              href={'/dashboard/pots'}
+              className='flex gap-1 hover:border-b border-blue-500 transition-all duration-300'
+            >
+              See Details <ChevronRight />
+            </Link>
+          </div>
+          <div className='flex'>
+            <div className='border-2  rounded-lg p-4 mr-2'>
+              <h2 className=''>Total Saved</h2>
+              <span className='text-xl font-semibold'>${saved.toFixed(2)}</span>
+            </div>
+            <div className='grid grid-cols-2 gap-2'>
+              {transactions
+                .filter((t) => t.saved > 0)
+                .map((t, i) => (
+                  <div
+                    key={t.description}
+                    className={cn(
+                      'border-2 border-l-blue-500 rounded-lg p-2',
+                      i % 2 !== 0 ? 'border-l-yellow-500' : ''
+                    )}
+                  >
+                    <h2 className='text-sm'>{t.description}</h2>
+                    <span className=''>${t.saved.toFixed(2)}</span>
+                  </div>
+                ))}
+            </div>
+          </div>
+        </div>
+        <div>
+        <div className='flex flex-col'>
+          <div className='flex items-center justify-between py-2'>
+            <h1 className='text-xl font-semibold'>Budgets</h1>
+            <Link
+              href={'/dashboard/budgets'}
+              className='flex gap-1 hover:border-b border-blue-500 transition-all duration-300'
+            >
+              See Details <ChevronRight />
+            </Link>
+          </div>
+          </div>
+          <ChartPie />
+        </div>
+      </div>
+    </main>
   )
 }
 
---
+++ components/ChartPie.tsx
@@ -0,0 +1,127 @@
+"use client"
+
+import * as React from "react"
+import { TrendingUp } from "lucide-react"
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+import transactions from '@/data/data.json'
+
+
+const income = transactions
+    .filter((t) => t.type === 'income')
+    .reduce((acc, t) => acc + t.amount, 0)
+
+    const entertainment = transactions
+    .filter((e)=>e.category==='entertainment')
+    .reduce((acc, t) => acc + t.amount, 0)
+   const diningOut= transactions
+   .filter((e)=>e.category==='dining out')
+   .reduce((acc, t) => acc + t.amount, 0)
+   const groceries= transactions
+   .filter((e)=>e.category==='groceries')
+   .reduce((acc, t) => acc + t.amount, 0)
+
+const chartData = [
+  { browser: "income", visitors: income, fill: "var(--color-income)" },
+  { browser: "entertainment", visitors: entertainment, fill: "var(--color-entertainment)" },
+  { browser: "diningOut", visitors: diningOut, fill: "var(--color-diningOut)" },
+  { browser: "groceries", visitors: groceries, fill: "var(--color-groceries)" },
+]
+
+const chartConfig = {
+  visitors: {
+    label: "Visitors",
+  },
+  income: {
+    label: "Income",
+    color: "hsl(var(--chart-1))",
+  },
+ 
+  entertainment: {
+    label: "Entertainment",
+    color: "hsl(var(--chart-2))",
+  },
+  diningOut: {
+    label: "Dining Out",
+    color: "hsl(var(--chart-3))",
+  },
+  groceries: {
+    label: "Groceries",
+    color: "hsl(var(--chart-4))",
+  },
+} satisfies ChartConfig
+
+export function ChartPie() {
+  const totalVisitors = React.useMemo(() => {
+    return (income-(entertainment+diningOut+groceries)).toFixed(2)
+  }, [])
+
+  return (
+    <Card className="flex flex-col">
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer
+          config={chartConfig}
+          className="mx-auto aspect-square max-h-[250px]"
+        >
+          <PieChart>
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Pie
+              data={chartData}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={60}
+              strokeWidth={5}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        textAnchor="middle"
+                        dominantBaseline="middle"
+                      >
+                        <tspan
+                          x={viewBox.cx}
+                          y={viewBox.cy}
+                          className="fill-foreground text-2xl font-semibold"
+                        >
+                          {totalVisitors.toLocaleString()}
+                        </tspan>
+                        <tspan
+                          x={viewBox.cx}
+                          y={(viewBox.cy || 0) + 24}
+                          className="fill-muted-foreground"
+                        >
+                          Balance
+                        </tspan>
+                      </text>
+                    )
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
